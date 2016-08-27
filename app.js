9995c79fa3da608b25ee9448243f7f165aec2c4d
titulus <titulus.web@gmail.com>
2016-08-27 12:32:46 +0300

    make localstorage model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ViewFile = require('./view-file');
 const ModelLS = require('./model-localstorage');
 
 let v = new ViewFile('f');
-let m = new ModelLS();
+let m = new ModelLS('./localstorage');
 let c = new Controller(m,v);
 c.sayhi = function() {
 	this.v.write('Hi');
@@ -15,8 +15,17 @@ c.ask = function(question) {
 	if (question) this.v.write(question);
 	console.log('answer is:',this.v.read());
 };
+c.extract = function(key) {
+	console.log(this.m.get(key));
+};
+c.store = function(key,value) {
+	this.m.post(key,value);
+};
+
 console.log(c,v);
 c.sayhi();
 c.ask();
 c.ask('what?');
 c.ask();
+c.store('a',1);
+c.extract('a');
--- model-localstorage.js
+++ model-localstorage.js
@@ -1,13 +1,18 @@
 'use strict';
-const LocalStorage = require('node-localstorage').LocalStorage;
-const localStorage = new LocalStorage('./scratch');
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  var localStorage;
+};
 
 class Model {
+	constructor(path) {
+		localStorage = new LocalStorage(path);
+	};
 	post(key,value) {
-		LocalStorage.setItem(key,value);
+		localStorage.setItem(key,value);
 	};
 	get(key) {
-		LocalStorage.getItem(key);
+		return localStorage.getItem(key);
 	};
 };
 
