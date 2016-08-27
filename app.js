8bcb888e2a016917ca75fa06c82813ee071c48d6
titulus <titulus.web@gmail.com>
2016-08-27 12:42:07 +0300

    ref

--- app.js
+++ app.js
@@ -5,27 +5,19 @@ const ViewSTDIO = require('./view-stdio');
 const ViewFile = require('./view-file');
 const ModelLS = require('./model-localstorage');
 
-let v = new ViewFile('f');
 let m = new ModelLS('./localstorage');
+let v = new ViewSTDIO('f');
 let c = new Controller(m,v);
 c.sayhi = function() {
 	this.v.write('Hi');
 };
-c.ask = function(question) {
-	if (question) this.v.write(question);
-	console.log('answer is:',this.v.read());
-};
-c.extract = function(key) {
-	console.log(this.m.get(key));
-};
 c.store = function(key,value) {
 	this.m.post(key,value);
 };
-
-console.log(c,v);
+c.show_val = function(key) {
+	let val = this.m.get(key);
+	this.v.write(val);
+};
 c.sayhi();
-c.ask();
-c.ask('what?');
-c.ask();
 c.store('a',1);
-c.extract('a');
+c.show_val('a');
