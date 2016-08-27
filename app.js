206ea6d22497af36e197378f988439510ae19226
titulus <titulus.web@gmail.com>
2016-08-27 11:40:43 +0300

    add file view

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 
 const Controller = require('./controller');
 const ViewSTDIO = require('./view-stdio');
+const ViewFile = require('./view-file');
 
-let v = new ViewSTDIO();
+let v = new ViewFile('f');
 let c = new Controller(1,v);
 c.sayhi = function() {
 	this.v.write('Hi');
 };
 c.ask = function(question) {
-	this.v.write(question);
+	if (question) this.v.write(question);
 	console.log('answer is:',this.v.read());
-}
-
+};
 console.log(c,v);
 c.sayhi();
+c.ask();
 c.ask('what?');
 c.ask();
